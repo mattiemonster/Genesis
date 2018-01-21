57b5998c0a907a06c98caac7064f8990b6607b9b
Mattie <mattiemonster1@hotmail.com>
2018-01-21 16:01:36 +0000

    Added a quit button to the main menu.

--- src/com/thecherno/ld24/menu/MainMenu.java
+++ src/com/thecherno/ld24/menu/MainMenu.java
@@ -13,30 +13,36 @@ public class MainMenu extends Menu {
 	}
 
 	int timer = 18;
-	String[] options = { "Play", "Help", "About" };
+	String[] options = { "Play", "Help", "About", "Quit" };
 	int selected = 0;
 
 	public void update() {
 		if (timer > 0) timer--;
-		if (input.down && selected < options.length && timer == 0) {
+		if (input.down && timer == 0) {
 			selected++;
+			if (selected == 4)
+				selected = 0;
 			Sound.menucycle.play(false);
 			timer = 10;
 		}
-		if (input.up && selected > 0 && timer == 0) {
+		
+		if (input.up && timer == 0) {
 			selected--;
+			if (selected == -1)
+				selected = 3;
 			Sound.menucycle.play(false);
 			timer = 10;
 		}
 
 		if (selected < 0) selected = 0;
-		if (selected > 2) selected = 2;
+		if (selected > 3) selected = 3;
 
 		if (selected == 0) {
 			options[selected] = "> " + "Play" + " <";
 		} else {
 			options[0] = "Play";
 		}
+		
 		if (selected == 1) {
 			options[selected] = "> " + "Help" + " <";
 
@@ -47,6 +53,7 @@ public class MainMenu extends Menu {
 		} else {
 			options[1] = "Help";
 		}
+		
 		if (selected == 2) {
 			options[selected] = "> " + "About" + " <";
 			if (input.use) {
@@ -55,6 +62,19 @@ public class MainMenu extends Menu {
 			}
 		} else {
 			options[2] = "About ";
+		}
+		
+		if (selected == 3) {
+			options[selected] = "> " + "Quit" + " <";
+			if (input.use) {
+//				Sound.menu.play(false);
+//				Game.menu = new AboutMenu(input);
+				
+				System.exit(0);
+				System.out.println("Quiting...");
+			}
+		} else {
+			options[3] = "Quit ";
 		}
 
 		if (selected == 0 && input.use && timer == 0) {
@@ -71,11 +91,20 @@ public class MainMenu extends Menu {
 	public void render(Screen screen) {
 		screen.renderText("Genesis", 170 + 4, 140 + 4, 120, 1, 0);
 		screen.renderText("Genesis", 170, 140, 120, 1, 0xffffff);
-		screen.renderText("A game by Yan Chernikov.", 250 + 2, 188 + 2, 30, 1, 0);
-		screen.renderText("A game by Yan Chernikov.", 250, 188, 30, 1, 0xffffff);
+		screen.renderText("A game by Yan Chernikov. Edited by Mattie.", 75 + 2, 188 + 2, 30, 1, 0);
+		screen.renderText("A game by Yan Chernikov. Edited by Mattie.", 75, 188, 30, 1, 0xffffff);
 		for (int i = 0; i < options.length; i++) {
-			screen.renderText(options[i], 350 + 3, 350 + i * 60 + 3, 50, 1, 0);
-			screen.renderText(options[i], 350, 350 + i * 60, 50, 1, 0xffffff);
+			if (i != 3 && i != 0)
+			{
+				screen.renderText(options[i], 350 + 3, 300 + i * 60 + 3, 50, 1, 0);
+				screen.renderText(options[i], 350, 300 + i * 60, 50, 1, 0xffffff);
+			} else if (i == 3) {
+				screen.renderText(options[i], 350 + 3, 300 + i * 60 + 3, 50, 1, 0);
+				screen.renderText(options[i], 350, 300 + i * 60, 50, 1, 0xff0000);
+			} else if (i == 0) {
+				screen.renderText(options[i], 350 + 3, 300 + i * 60 + 3, 50, 1, 0);
+				screen.renderText(options[i], 350, 300 + i * 60, 50, 1, 0x00ff00);
+			}
 		}
 	}
 }
